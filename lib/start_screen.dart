a6dc82c949b99e5ba1e8ccc7aa8c4cb4c8d7c584
neloy-mamun <neloymamun06@gmail.com>
2025-03-21 00:23:15 +0600

    add image and icon Button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,29 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Hello'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          SizedBox(height: 50),
+          Text(
+            'Learn Flutter the Fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24.0),
+          ),
+          SizedBox(height: 50),
+          OutlinedButton.icon(
+            onPressed: () {},
+
+            style: OutlinedButton.styleFrom(
+              iconSize: 50,
+              foregroundColor: Colors.white,
+            ),
+            icon: Icon(Icons.arrow_right_alt),
+            label: Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
