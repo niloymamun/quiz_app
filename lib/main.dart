a964bf3e7204c6583f371d88741f6aa62d909fd2
neloy-mamun <neloymamun06@gmail.com>
2025-03-20 12:33:05 +0600

    add LinearGradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:quiz/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
